5e8b1506d3a90773b9ee26d082ea7c4eefd4cd11
Denis Zhokhov <d3z41k@gmail.com>
2020-10-29 11:26:43 +0200

    Add abstract factory pattern

--- factory/factory/factory.go
+++ factory/factory/factory.go
@@ -11,10 +11,30 @@ type (
 		database map[string]string
 	}
 
+	file struct {
+		name    string
+		content string
+	}
+
+	ntfs struct {
+		files map[string]file
+	}
+
+	ext4 struct {
+		files map[string]file
+	}
+
+	FileSystem interface {
+		CreateFile(string)
+		FindFile(string) file
+	}
+
 	Database interface {
 		GetData(string) string
 		PutData(string, string)
 	}
+
+	Factory func(string) interface{}
 )
 
 func (mdb mongoDB) GetData(query string) string {
@@ -43,7 +63,35 @@ func (sql sqlite) PutData(query string, data string) {
 	sql.database[query] = data
 }
 
-func DatabaseFactory(env string) Database {
+func (ntfs ntfs) CreateFile(path string) {
+	file := file{content: "NTFS file", name: path}
+	ntfs.files[path] = file
+	fmt.Println("NTFS")
+}
+
+func (ext ext4) CreateFile(path string) {
+	file := file{content: "EXT4 file", name: path}
+	ext.files[path] = file
+	fmt.Println("EXT4")
+}
+
+func (ntfs ntfs) FindFile(path string) file {
+	if _, ok := ntfs.files[path]; !ok {
+		return file{}
+	}
+
+	return ntfs.files[path]
+}
+
+func (ext ext4) FindFile(path string) file {
+	if _, ok := ext.files[path]; !ok {
+		return file{}
+	}
+
+	return ext.files[path]
+}
+
+func DatabaseFactory(env string) interface{} {
 	switch env {
 	case "production":
 		return mongoDB{
@@ -53,6 +101,32 @@ func DatabaseFactory(env string) Database {
 		return sqlite{
 			database: make(map[string]string),
 		}
+	default:
+		return nil
+	}
+}
+
+func FilesystemFactory(env string) interface{} {
+	switch env {
+	case "production":
+		return ntfs{
+			files: make(map[string]file),
+		}
+	case "development":
+		return ext4{
+			files: make(map[string]file),
+		}
+	default:
+		return nil
+	}
+}
+
+func AbstractFactory(fact string) Factory {
+	switch fact {
+	case "database":
+		return DatabaseFactory
+	case "filesystem":
+		return FilesystemFactory
 	default:
 		return nil
 	}
--- factory/main.go
+++ factory/main.go
@@ -6,12 +6,19 @@ import (
 	"reflect"
 )
 
+func SetupConstructors(env string) (factory.Database, factory.FileSystem) {
+	fs := factory.AbstractFactory("filesystem")
+	db := factory.AbstractFactory("database")
+
+	return db(env).(factory.Database), fs(env).(factory.FileSystem)
+}
+
 func main() {
 	env1 := "production"
 	env2 := "development"
 
-	db1 := factory.DatabaseFactory(env1)
-	db2 := factory.DatabaseFactory(env2)
+	db1, fs1 := SetupConstructors(env1)
+	db2, fs2 := SetupConstructors(env2)
 
 	db1.PutData("test", "this is mongodb")
 	fmt.Println(db1.GetData("test"))
@@ -19,8 +26,19 @@ func main() {
 	db2.PutData("test", "this is sqlite")
 	fmt.Println(db2.GetData("test"))
 
+	fs1.CreateFile("../example/testntfs.txt")
+	fmt.Println(fs1.FindFile("../example/testntfs.txt"))
+
+	fs2.CreateFile("../example/testext4.txt")
+	fmt.Println(fs2.FindFile("../example/testext4.txt"))
+
 	fmt.Println(reflect.TypeOf(db1).Name())
 	fmt.Println(reflect.TypeOf(&db1).Elem())
 	fmt.Println(reflect.TypeOf(db2).Name())
 	fmt.Println(reflect.TypeOf(&db2).Elem())
+
+	fmt.Println(reflect.TypeOf(fs1).Name())
+	fmt.Println(reflect.TypeOf(&fs1).Elem())
+	fmt.Println(reflect.TypeOf(fs2).Name())
+	fmt.Println(reflect.TypeOf(&fs2).Elem())
 }
